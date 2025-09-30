ba902f0c88ff9b18a3decb79a33a68d1237cc72d
EPIC\thilina_h <thilina_h@epiclanka.net>
2025-09-30 16:24:00 +0530

    ss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Dashboard } from './dashboard/dashboard';
+import { Client } from './client/client';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/client', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'client', component: Client },
+  { path: 'register', component: Register }
+];
