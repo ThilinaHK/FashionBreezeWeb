4d9310acf2405cbd8e695cc9b0a26f611e3dfbbe
EPIC\thilina_h <thilina_h@epiclanka.net>
2025-10-04 18:16:56 +0530

    final initial 12

--- app/api/auth/user/route.ts
+++ app/api/auth/user/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import dbConnect from '../../../lib/mongodb';
 import Customer from '../../../lib/models/Customer';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   try {
     await dbConnect();
